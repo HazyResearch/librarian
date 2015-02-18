24dc52fcc3be25390f0b5d2f39ba8779787855ec
abhirast <arastogi@stanford.edu>
2015-02-17 16:23:32 -0800

    Adding database.py, which helps in using google sheets as a database

--- librarian/database.py
+++ librarian/database.py
@@ -1,7 +1,18 @@
+######################################################################################
+#                                                                                    #
+# A temprorary implementation which uses google sheets as the database for librarian #
+#                                                                                    #
+#                                                                                    #
+# Written by - Abhinav Rastogi, 02/16/2015                                           #
+#                                                                                    #
+######################################################################################
+
+
 import os
 from urllib2 import urlopen
 from subprocess import call
 import json
+import re
 
 read_key = os.getenv('SHEETSDB_KEY_READ')
 
@@ -15,32 +26,61 @@ def insert(table, **kwargs):
 
 
 # Query from sheet based on an attribute
-def query(table, numcols, **kwargs):
+def query(table, **kwargs):
     row = dict(kwargs)
     params = []
     for k, v in row.iteritems():
         params.append(k + '%3D' + repr(v))
-    query = 'select%20' + '%2C'.join(map(chr, range(ord('A'),ord('A')+numcols))) 
+    query = 'select%20*' 
     query += '%20where%20' + '%20and%20'.join(params)
     url = 'https://docs.google.com/spreadsheets/d/' + read_key + \
         '/gviz/tq?tq=' + query + '&sheet=' + table
     resp = urlopen(url)
-    res = json.loads(resp.read()[39:-2])
+    res = resp.read()[39:-2]
+    print res
+    # Parse the javascript dates
+    hasDate = re.search(r'new Date\([0-9,]*\)', res)
+    while hasDate:
+        a,b = hasDate.span()
+        y, m, d = map(int, res[a+9:b-1].split(','))
+        res = res[:a]+'"'+str(m+1)+'-'+str(d)+'-'+str(y)+'"'+res[b:]
+        hasDate = re.search(r'new Date\([0-9,]*\)', res)
+    # load the json object
+    res = json.loads(res)
     cols = map(lambda x:x[u'label'], res[u'table'][u'cols'])
     rows = [map(lambda x:x[u'v'], r[u'c']) for r in res[u'table'][u'rows']]
     return cols, rows
     
+# id, name, date_originated, owner, comments
+def add_Engagement(id, name, date, owner, comments):
+    args_dict = {'id':id, 'name':name, 'date_originated':date, 
+        'owner':owner, 'comments':comments}
+    insert('Engagements', **args_dict)
 
-def add_Engagement():
-    pass
-
-def add_Incoming():
-    pass
+# engagement_id, name, version, part, incoming_id, date_ingested, checksum, s3path, comments
+def add_Incoming(id, name, ver, part, in_id, date, checksum, s3, comments):
+    args_dict = {'engagement_id':id, 'name':name, 'version':ver, 
+        'part':part, 'incoming_id':in_id, 'date_ingested':date, 
+        'checksum':checksum, 's3path':s3, 'comments':comments} 
+    insert('IncomingData', **args_dict)
     
-def add_Outgoing():
-    pass
-    
-def add_ActivityLog():
-    pass
+# engagement_id, name, version, outgoing_id, date_produced, checksum, s3path, published_report_s3path, comments  
+def add_Outgoing(id, name, ver, out_id, date, checksum, s3, report, comments):
+    args_dict = {'engagement_id':id, 'name':name, 'version':ver, 'outgoing_id':out_id, 
+        'date_produced':date, 'checksum':checksum, 's3path':s3, 
+        'published_report_s3path':report, 'comments':comments}
+    insert('OutgoingData', **args_dict)
 
-print query('test', 2, A=11)
+# action_id, action_type, date, description, engagement_id, incoming_id, outoging_id
+def add_ActivityLog(act_id, act_type, date, desc, eng_id, inc_id, out_id):
+    args_dict = {'action_id':act_id, 'action_type':act_type, 'date':date, 
+        'description':desc, 'engagement_id':eng_id, 'incoming_id':inc_id, 
+        'outoging_id':out_id}
+    insert('ActivityLog', **args_dict)
+
+if __name__=='__main__':
+    # add_Engagement(0, 'memex', 'Sept-1-2014', 'mjc', 'This will be big!')
+    # add_Incoming(0, 'small sample of backpage', 0, 0, 0, 'Sept-5-2014', 'MD5...', 's3:dd-incoming/memex/small_sample_of_backpage/v_0000/data.tsv', 'more data expected later')
+    # add_Outgoing(0, 'Memex ht basic attrs', 0, 0, 'Nov-30-2014', 'MD5:...', 's3:dd-outgoing/memex/memex_ht_basic_attrs/v_0000/data_0000.tsv', 's3:dd-outgoing/memex/memex_ht_basic_attrs/v_0000/report.pdf', 'First shipment!  This reflects input backpage data from the large-backpage-crawl up through Nov-1-2014')
+    # add_ActivityLog(0, 'CREATE', 'Jan-20-2015', 'Add new engagement', 3, 'NULL', 'NULL')
+    print query('ActivityLog', A=0)
--- librarian/storage_s3.py
+++ librarian/storage_s3.py
@@ -1,6 +1,43 @@
 #!/usr/bin/env python
 # upload-s3.py -- Librarian script that takes care of uploading data to AWS S3
 
-def upload(local_paths, remote_path):
-    # TODO
-    raise Exception("Not implemented yet")
+import boto
+import boto.s3.connection
+import os
+
+def upload(local_path, project, name):
+    # get the keys
+    access_key = os.getenv('AWS_KEY')
+    secret_key = os.getenv('AWS_SECRET_KEY')
+
+    conn = boto.s3.connection.S3Connection(
+        aws_access_key_id = access_key,
+        aws_secret_access_key = secret_key,
+        calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+        )
+    
+    # each project has its own bucket
+    bucket = conn.lookup(project)
+    if bucket is None:
+        print 'Error bucket was owned\n\n'
+        bucket = conn.create_bucket(project)
+        key = bucket.new_key(name)
+    else:
+        key = bucket.get_key(name)
+        i = 0
+        while key is not None:
+            i += 1
+            key = bucket.get_key(name+'_'+str(i))
+        key = bucket.new_key(name+'_'+str(i))
+    # check if a file with the same name exists. If yes, generate a new name
+    # Can replace this scheme by appending a random string to the end if a large
+    # number of files is expected
+    
+        
+    key.set_contents_from_filename(local_path)
+    key.set_acl('public-read')
+    url = key.generate_url(expires_in=0, query_auth=False)
+    return url
+    
+if __name__=='__main__':
+    print upload('/home/abhinav/librarian/librarian/boto_test.py', 'arastogi@stanford.edu', 'test1')
